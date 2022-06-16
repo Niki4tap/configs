2014ad20515f54a291246f26564d98a4d383e104
Niki4tap <rombiklol2@gmail.com>
2022-06-16 10:34:39 +0300

    Fix formatting

--- common/editors.nix
+++ common/editors.nix
@@ -3,10 +3,10 @@
 with util-lib; pkgDef {
 	def_name = "main.editors";
 	def = with pkgs; {
-		clion		= [(mkOptIn		"Install clion.")		[jetbrains.clion]];
-		pycharm		= [(mkOptIn		"Install pycharm.")		[jetbrains.pycharm-community]];
-		vscode		= [(mkOptOut	"Install vscode.")		[vscode-fhs]];
-		kdenlive	= [(mkOptIn		"Install kdenlive.")	[kdenlive]];
+		clion		= [(mkOptIn	"Install clion.")	[jetbrains.clion]];
+		pycharm		= [(mkOptIn	"Install pycharm.")	[jetbrains.pycharm-community]];
+		vscode		= [(mkOptOut	"Install vscode.")	[vscode-fhs]];
+		kdenlive	= [(mkOptIn	"Install kdenlive.")	[kdenlive]];
 	};
 	cfg = config;
 }
--- common/lang.nix
+++ common/lang.nix
@@ -5,13 +5,13 @@ let
 in with util-lib; pkgDef {
 	def_name = "main.lang";
 	def = with pkgs; {
-		llvm_extra		= [(mkOptIn		"Install llvm tools.")				[llvmPackages.bintools-unwrapped]];
-		gcc				= [(mkOptIn		"Install gcc.")						[gcc]];
-		clang			= [(mkOptOut	"Install clang.")					[clang]];
-		python			= [(mkOptOut	"Install python v3.10.")			[python310]];
+		llvm_extra	= [(mkOptIn	"Install llvm tools.")			[llvmPackages.bintools-unwrapped]];
+		gcc		= [(mkOptIn	"Install gcc.")				[gcc]];
+		clang		= [(mkOptOut	"Install clang.")			[clang]];
+		python		= [(mkOptOut	"Install python v3.10.")		[python310]];
 		python_extra	= [(mkOptOut	"Install extra libs for python.")	[python310Packages.requests python310Packages.pip]];
-		rust			= [(mkOptIn		"Install rust.")					[rust-latest-nightly rustup]];
-		java8			= [(mkOptIn		"Install java v8.")					[jre8]];
+		rust		= [(mkOptIn	"Install rust.")			[rust-latest-nightly rustup]];
+		java8		= [(mkOptIn	"Install java v8.")				[jre8]];
 	};
 	cfg = config;
 }
