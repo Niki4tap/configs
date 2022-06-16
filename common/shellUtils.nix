28e122b604166b07629f3a975f240e303071bbaa
Niki4tap <rombiklol2@gmail.com>
2022-06-16 10:48:38 +0300

    Fix formatting for real

--- common/shellUtils.nix
+++ common/shellUtils.nix
@@ -5,16 +5,16 @@ with util-lib; pkgDef {
 	def = with pkgs; {
 		cool_looking	= [(mkOptOut	"Install cool looking programs.")	[neofetch cpufetch lolcat bat]];
 		basics		= [(mkOptOut	"Install basic shell utilities.")	[pciutils file wget]];
-		git		= [(mkOptOut	"Install git.")						[git]];
-		earlyoom	= [(mkOptIn	"Install earlyoom.")				[earlyoom]];
-		xclip		= [(mkOptIn	"Install xclip.")					[xclip]];
-		ffmpeg		= [(mkOptIn	"Install ffmpeg.")					[ffmpeg-full]];
-		nmap		= [(mkOptOut	"Install nmap.")					[nmap]];
-		dig		= [(mkOptOut	"Install dig.")						[dig]];
-		htop		= [(mkOptIn	"Install htop.")					[htop]];
-		bpytop		= [(mkOptIn	"Install bpytop.")					[bpytop]];
-		tmux		= [(mkOptOut	"Install tmux.")					[tmux]];
-		screen		= [(mkOptIn	"Install screen.")					[screen]];
+		git		= [(mkOptOut	"Install git.")				[git]];
+		earlyoom	= [(mkOptIn	"Install earlyoom.")			[earlyoom]];
+		xclip		= [(mkOptIn	"Install xclip.")			[xclip]];
+		ffmpeg		= [(mkOptIn	"Install ffmpeg.")			[ffmpeg-full]];
+		nmap		= [(mkOptOut	"Install nmap.")			[nmap]];
+		dig		= [(mkOptOut	"Install dig.")				[dig]];
+		htop		= [(mkOptIn	"Install htop.")			[htop]];
+		bpytop		= [(mkOptIn	"Install bpytop.")			[bpytop]];
+		tmux		= [(mkOptOut	"Install tmux.")			[tmux]];
+		screen		= [(mkOptIn	"Install screen.")			[screen]];
 	};
 	cfg = config;
 }
