9ad61e6bf64f8e99f570bc6b0de526ced8ad348d
Niki4tap <rombiklol2@gmail.com>
2022-07-29 00:34:54 +0300

    Refactor fish.nix

--- common/fish.nix
+++ common/fish.nix
@@ -1,37 +1,28 @@
 {config, pkgs, util-lib, ...}:
 
 let
-	cfg = config.main.fish;
 	cnf-fix-sh = pkgs.writeShellScriptBin "cnf-fix" ''
-		sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos && sudo nix-channel --update
-	'';
-	omf-install-sh = pkgs.writeShellScriptBin "omf-install" ''
-		curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/v7/bin/install | fish
-	'';
-in {
-	options.main.fish = with util-lib; {
-		fish = mkOptOut "Install fish shell.";
-		omf = mkOptIn "Install oh my fish.";
-		cnf-fix = mkOptOut "Make command-not-found fix script.";
-	};
-	config = util-lib.mkMerge [
-		{
-			_condition = cfg.fish;
+ 		sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos && sudo nix-channel --update
+ 	'';
+ 	omf-install-sh = pkgs.writeShellScriptBin "omf-install" ''
+ 		curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/v7/bin/install | fish
+ 	'';
+in with util-lib; cfgDef {
+	def_name = "main.fish";
+	def = {
+		fish = [(mkOptOut "Install fish shell.") {
 			programs.fish.enable = true;
-		}
-
-		{
-			_condition = cfg.omf;
+		}];
+		omf = [(mkOptIn "Install oh my fish.") {
 			environment.systemPackages = [
 				omf-install-sh # Not using oh-my-fish package, it's broken
 			];
-		}
-
-		{
-			_condition = cfg.cnf-fix;
+		}];
+		cnf-fix = [(mkOptOut "Make command-not-found fix script.") {
 			environment.systemPackages = [
 				cnf-fix-sh
 			];
-		}
-	];
+		}];
+	};
+	inherit config;
 }
