fd5d7d5d8fe83b03f5b923d443004a6538baf8b7
Niki4tap <rombiklol2@gmail.com>
2022-10-22 10:21:19 +0300

    how was this even working???

--- common/communication.nix
+++ common/communication.nix
@@ -5,7 +5,7 @@ let
 		withOpenASAR = true;
 		version = "0.0.21";
 		src = pkgs.fetchurl {
-			url = "https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.21.tar.gz";
+			url = "https://dl.discordapp.net/apps/linux/0.0.21/discord-0.0.21.tar.gz";
 			sha256 = "18rmw979vg8lxxvagji6sim2s5yyfq91lfabsz1wzbniqfr98ci8";
 		};
 	});
