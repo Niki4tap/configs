3b4bce1f56a4acf5f583cfec874f591d6f8645be
Niki4tap <rombiklol2@gmail.com>
2022-10-22 10:13:15 +0300

    Update discord

--- common/communication.nix
+++ common/communication.nix
@@ -5,8 +5,8 @@ let
 		withOpenASAR = true;
 		version = "0.0.20";
 		src = pkgs.fetchurl {
-			url = "https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.20.tar.gz";
-			sha256 = "1355xiw0k0i6cbi1zcq7fphma6r8hz87lhm8ybg5s45032xz5znx";
+			url = "https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.21.tar.gz";
+			sha256 = "18rmw979vg8lxxvagji6sim2s5yyfq91lfabsz1wzbniqfr98ci8";
 		};
 	});
 in with util-lib; pkgDef {
