b3c922a721a5a3f74825a0e2dbe96a4163b48797
Niki4tap <rombiklol2@gmail.com>
2023-01-02 13:36:52 +0300

    code -> codium

--- common/editors.nix
+++ common/editors.nix
@@ -3,9 +3,9 @@
 with util-lib; pkgDef {
 	def_name = "main.editors";
 	def = with pkgs; {
+		codium		= [(mkOptOut	"Install codium.")	[vscodium]];
 		clion		= [(mkOptIn	"Install clion.")	[jetbrains.clion]];
 		pycharm		= [(mkOptIn	"Install pycharm.")	[jetbrains.pycharm-community]];
-		vscode		= [(mkOptOut	"Install vscode.")	[vscode-fhs]];
 		kdenlive	= [(mkOptIn	"Install kdenlive.")	[kdenlive]];
 		krita		= [(mkOptOut	"Install krita.")	[krita]];
 	};
