eb3f8e69b38b9759ddcb19c8613d44251216c0ad
Niki4tap <rombiklol2@gmail.com>
2023-02-24 22:50:30 +0300

    shellutils

--- common/shellUtils.nix
+++ common/shellUtils.nix
@@ -5,16 +5,19 @@ with util-lib; pkgDef {
 	def = with pkgs; {
 		dbg		= [(mkOptOut	"Install debugging programs.")		[gdb]];
 		cool_looking	= [(mkOptOut	"Install cool looking programs.")	[neofetch cpufetch lolcat bat icdiff]];
-		basics		= [(mkOptOut	"Install basic shell utilities.")	[pciutils file wget curl ripgrep]];
+		basics		= [(mkOptOut	"Install basic shell utilities.")	[pciutils file wget curl ripgrep hexedit imagemagick]];
 		git		= [(mkOptOut	"Install git.")				[git]];
 		earlyoom	= [(mkOptIn	"Install earlyoom.")			[earlyoom]];
-		xclip		= [(mkOptIn	"Install xclip.")			[xclip]];
+		xclip		= [(mkOptOut	"Install xclip.")			[xclip]];
 		ffmpeg		= [(mkOptIn	"Install ffmpeg.")			[ffmpeg-full]];
 		nmap		= [(mkOptOut	"Install nmap.")			[nmap]];
 		dig		= [(mkOptOut	"Install dig.")				[dig]];
-		htop		= [(mkOptIn	"Install htop.")			[htop]];
+		htop		= [(mkOptOut	"Install htop.")			[htop]];
 		bpytop		= [(mkOptIn	"Install bpytop.")			[bpytop]];
-		tmux		= [(mkOptOut	"Install tmux.")			[tmux]];
+		# Let's see how this goes?
+		zellij		= [(mkOptOut	"Install zellij.")			[zellij]];
+		# sorry tmux, sorry screen :)
+		tmux		= [(mkOptIn	"Install tmux.")			[tmux]];
 		screen		= [(mkOptIn	"Install screen.")			[screen]];
 	};
 	inherit config;
